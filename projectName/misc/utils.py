96aa250b6bd680eb261f9f4b35e91d19a536f9b7
Kenneth Brezinski <34727483+kbrezinski@users.noreply.github.com>
2021-04-06 00:52:23 -0500

    Finalized changes to trainer template

--- projectName/misc/utils.py
+++ projectName/misc/utils.py
@@ -2,6 +2,33 @@
 import time
 import os
 
+def update_model():
+
+    snapshot_name = "{} {} {}".format(epoch + 1, mae, mse)
+
+    if mae < train_record['best_mae']:
+        train_record['best_model_name'] = snapshot_name
+        saved_weights = net.state_dict()
+        torch.save(saved_weights,
+                    path + exp + snapshot_name + ".pth")
+
+    ## adjust train record for best metrics
+
+    latest_state = {
+        'train_record' : train_record,
+        'net' : net.state_dict,
+        'optimizer' : optimizer.state_dict(),
+        'scheduler' : scheduler.state_dict(),
+        'epoch' : epoch,
+        'exp_path' : exp_path,
+        'exp_name' : exp_name
+        }
+
+    torch.save(latest_state, path + exp + "latest_state.pth")
+
+    return train_record
+
+
 def logger(exp_path, exp_name):
     from tensorboardx import SummaryWriter
 
@@ -11,7 +38,6 @@ def logger(exp_path, exp_name):
     writer = SummaryWriter(exp_path + "/" + exp_name)
 
     return writer
-
 
 class Timer:
 
@@ -34,3 +60,20 @@ class Timer:
             return self.average_time
         else:
             return self.diff
+
+class AverageMeter:
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.cur_val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, cur_val):
+        self.cur_val = cur_val
+        self.sum += cur_val
+        self.count += 1
+        self.avg = self.sum / self.count
--- projectName/trainer.py
+++ projectName/trainer.py
@@ -32,6 +32,9 @@ class Trainer:
         self.cfg_data = cfg_data
         self.pwd = pwd
 
+        self.epoch = 0
+        self.net = None ## import model.to(device)
+
         self.train_record = {
             'best_mae': math.inf,
             'best_model_name': ''}
@@ -42,12 +45,14 @@ class Trainer:
 
         self.train_loader, self.val_loader = self.loader(*params)
 
+        ## load previous state of model training
         if cfg.RESUME:
             latest_state = torch.load(cfg.RESUME_PATH)
             self.net.load_state_dict(latest_state['net'])
             self.optimizer.load_state_dict(latest_state['optimizer'])
             self.scheduler.load_state_dict(latest_state['scheduler'])
             self.epoch = latest_state['epoch'] + 1
+            self.train_record = latest_state['train_record']
             ## need to unload the model details
 
         self.writer = logger()
@@ -55,4 +60,81 @@ class Trainer:
 
     def forward(self):
 
-        for epoch in range(self.epoch, cfg.MAX_EPOCH)
+        for epoch in range(self.epoch, cfg.MAX_EPOCH):
+
+            self.epoch = epoch
+
+            self.timer['train_time'].tic()
+            self.train()
+            self.timer['train_time'].toc()
+
+            if epoch % cfg.VAL_FREQ == 0:
+                self.timer['val_time'].tic()
+                self.validate()
+                self.timer['val_time'].toc(average=False)
+
+
+    def train(self):
+        ## in torch activates batch norm, dropout, etc.
+        self.net.train()
+
+        for i, data in enumerate(self.train_loader):
+            self.timer['iter_time'].tic()
+            ## x, y = data
+
+            x = Variable(x).to(device)
+            y = Variable(y).to(device)
+
+            self.optimizer.zero_grad()
+            y_pred, loss = self.net(x, y)
+            loss.backward()
+
+            clip_grad_norm_(self.net.parameters(), None) ## grad clip clip_grad_norm_
+            self.optimizer.step()
+
+            ## Print update metrics
+            if (i + 1) % cfg.PRINT_FREQ == 0:
+                self.writer.add_scalar('train_loss', loss.item(), i) ## or other counter
+                self.timer['iter_time'].toc(average=False)
+                print(
+                    self.epoch,
+                    i + 1,
+                    loss.item(),
+                    self.optimizer.param_groups[0]['lr'],
+                    self.timer['iter_time'].diff()
+                )
+
+    def validate(self):
+
+        ## sets self.train(False)
+        self.net.eval()
+
+        losses = AverageMeter()
+        maes = AverageMeter()
+        mses = AverageMeter()
+
+        for vi, data in enumerate(self.val_loader):
+            ## x, y = data
+
+            with torch.no_grad():
+                x = Variable(x).to(device)
+                y = Variable(y).to(device)
+
+                y_pred, loss = self.net.test(x, y)
+
+                ## convert to numpy with .data.cpu().numpy()
+
+                for i in range(x.shape[0]):
+
+                    losses.update(loss)
+                    #maes.update()
+                    #mses.update()
+                #mae = maes.avg
+                #mse = np.sqrt(mses.avg)
+                #loss = losses.avg
+
+                self.writer.add_scalar('val_loss', loss, self.epoch + 1)
+                self.writer.add_scalar('mae', mae, self.epoch + 1)
+                self.writer.add_scalar('mse', mse, self.epoch + 1)
+
+                self.train_record = update_model()
